b51f94159122d6c367d915ca97f24b247f22fb75
AngelSweety05 <moviesadda.pro@gmail.com>
2025-12-30 22:02:43 +0530

    Update generate.py

--- lazydeveloperr/generate.py
+++ lazydeveloperr/generate.py
@@ -77,7 +77,9 @@ async def main(bot: Client, message: Message):
             uclient = Client(":memory:", session_string=string_session, api_id=API_ID, api_hash=API_HASH)
             await uclient.connect()
             await db.set_session(message.from_user.id, session=string_session)
+            await client.disconnect()
     except Exception as e:
         return await message.reply_text(f"<b>ERROR IN LOGIN:</b> `{e}`")
     await bot.send_message(message.from_user.id, "<b>Account Login Successfully.\n\nIf You Get Any Error Related To AUTH KEY Then /logout first and /login again</b>")
+
 
